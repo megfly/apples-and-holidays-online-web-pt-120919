703e2a8df8001c25473ddee469b6213a1e02c2e3
Meghan Flynn <mflynn333@gmail.com>
2019-12-21 01:32:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
-  
+ binding.pry 
 end
 
 def all_holidays_with_bbq(holiday_hash)
