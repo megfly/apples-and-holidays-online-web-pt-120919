95b516d7f3c1214076baf3440acc8f0b97ae5eb6
Meghan Flynn <mflynn333@gmail.com>
2019-12-21 02:04:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
  puts "#{season.to_s.capitalize}:"
   holiday.each do |holiday, supplies|
-    capitalized = holiday.to_s.split(' ').each {|supplies| supplies.capitalize!}.join(' ')
+    capitalized = holiday.to_s.split('_').each {|supplies| supplies.capitalize!}.join(' ')
     puts "  #{capitalized}: #{supplies.join(', ')}"
 end
 end 
